cc690378a7aaed6503403b98012c835451dbfd69
Zut2020 <pompos.balazs@gmail.com>
2022-04-04 19:59:06 +0200

    Commented the main terraform file Added comments for easier reading

--- main.tf
+++ main.tf
@@ -14,11 +14,13 @@ provider "azurerm" {
   features {}
 }
 
+#Add resource group
 resource "azurerm_resource_group" "rg" {
   name     = "VarianTask"
   location = "West Europe"
 }
 
+#Add virtual network
 resource "azurerm_virtual_network" "vn" {
   name                = "virtualnetwork"
   address_space       = ["10.0.0.0/16"]
@@ -26,6 +28,7 @@ resource "azurerm_virtual_network" "vn" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+#Add subnet
 resource "azurerm_subnet" "sn" {
   name                 = "subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -33,6 +36,7 @@ resource "azurerm_subnet" "sn" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+#Add public IP resource for the Linux VM
 resource "azurerm_public_ip" "linux" {
   name                = "linux-pip"
   resource_group_name = azurerm_resource_group.rg.name
@@ -40,6 +44,7 @@ resource "azurerm_public_ip" "linux" {
   allocation_method   = "Dynamic"
 }
 
+#Add public IP resource for the Windows VM
 resource "azurerm_public_ip" "windows" {
   name                = "windows-pip"
   resource_group_name = azurerm_resource_group.rg.name
@@ -47,6 +52,7 @@ resource "azurerm_public_ip" "windows" {
   allocation_method   = "Dynamic"
 }
 
+#Add NIC for Windows VM
 resource "azurerm_network_interface" "windows" {
   name                = "windowsNIC"
   location            = azurerm_resource_group.rg.location
@@ -60,6 +66,7 @@ resource "azurerm_network_interface" "windows" {
   }
 }
 
+#Add NIC for Linux VM
 resource "azurerm_network_interface" "linux" {
   name                = "LinuxNIC"
   location            = azurerm_resource_group.rg.location
@@ -73,8 +80,7 @@ resource "azurerm_network_interface" "linux" {
   }
 }
 
-
-
+#Create the Windows VM
 resource "azurerm_windows_virtual_machine" "windows" {
   name                = "windowsVM"
   resource_group_name = azurerm_resource_group.rg.name
@@ -99,6 +105,7 @@ resource "azurerm_windows_virtual_machine" "windows" {
   }
 }
 
+#Create the Linux VM
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "linuxVM"
   resource_group_name = azurerm_resource_group.rg.name
